07dc8fbfd8bbd903e3f108d50f84de0144991b7b
Lei Xu <leixu@leansoftx.com>
2025-03-26 21:29:28 -0700

    enable appInsights

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
+import { createBrowserHistory } from 'history';
+
+const browserHistory = createBrowserHistory();
+const reactPlugin = new ReactPlugin();
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString: 'InstrumentationKey=43485096-3cae-436a-84ea-6f813c67476b;IngestionEndpoint=https://southeastasia-1.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/;ApplicationId=14d289ca-9e97-4472-8636-8525948fce3c',
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: { history: browserHistory }
+    }
+  }
+});
+appInsights.loadAppInsights();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
