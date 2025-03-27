8b46b78ab91d8d35c0108f1f16379b958a074afd
Lei Xu <leixu@leansoftx.com>
2025-03-26 22:23:27 -0700

    add language switcher in header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,20 +5,23 @@ import { Docs } from './pages/Docs';
 import { About } from './pages/About';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { LanguageProvider } from './contexts/LanguageContext';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/docs" element={<Docs />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <LanguageProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/docs" element={<Docs />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </LanguageProvider>
   );
 }
 
